3255a245e936539cb456ed0d555989237c8607da
Keith Wannamaker <keith@wannamaker.org>
2021-11-05 16:36:55 -0400

    Don't reduce the length of repeated notes.

--- src/java/main/chimebox/midi/RepeatedNoteAdaptor.java
+++ src/java/main/chimebox/midi/RepeatedNoteAdaptor.java
@@ -1,20 +1,16 @@
 package chimebox.midi;
 
-import com.google.common.base.Preconditions;
-
 /**
  * Articulates repeated notes.  Reduces repeated note duration by amount of preceding silence added.
  */
 public class RepeatedNoteAdaptor implements PlayerInterface {
-  private static final int MIN_DELAY_BETWEEN_REPEATED_NOTES_MS = 50;
-  private static final int MIN_NOTE_LENGTH_MS = 20;
+  private static final int MIN_DELAY_BETWEEN_REPEATED_NOTES_MS = 150;
+  private static final int MIN_NOTE_LENGTH_MS = 150;
 
-  private int repeatedNoteReductionMs = 0;
   private int silenceDurationMs = 0;
   private int lastNoteOff = -1;
   private final PlayerInterface wrapped;
   private boolean silence = true;
-  private boolean playingRepeatedNote = false;
 
   public RepeatedNoteAdaptor(PlayerInterface wrapped) {
     this.wrapped = wrapped;
@@ -22,17 +18,9 @@ public class RepeatedNoteAdaptor implements PlayerInterface {
 
   @Override
   public void sleep(long durationMillis) {
-    if (playingRepeatedNote) {
-      Preconditions.checkState(!silence);
-      durationMillis -= Math.min(durationMillis, repeatedNoteReductionMs);
-      Preconditions.checkState(durationMillis >= 0);
-      durationMillis = Math.max(MIN_NOTE_LENGTH_MS, durationMillis);
-    }
     wrapped.sleep(durationMillis);
     if (silence) {
       silenceDurationMs += durationMillis;
-    } else if (playingRepeatedNote) {
-      repeatedNoteReductionMs = 0;
     }
   }
 
@@ -40,12 +28,9 @@ public class RepeatedNoteAdaptor implements PlayerInterface {
   public void noteOn(int midiNote) {
     if (lastNoteOff == midiNote) {
       if (silenceDurationMs < MIN_DELAY_BETWEEN_REPEATED_NOTES_MS) {
-        repeatedNoteReductionMs = MIN_DELAY_BETWEEN_REPEATED_NOTES_MS - silenceDurationMs;
+        int repeatedNoteReductionMs = MIN_DELAY_BETWEEN_REPEATED_NOTES_MS - silenceDurationMs;
         wrapped.sleep(repeatedNoteReductionMs);
-        playingRepeatedNote = true;
       }
-    } else {
-      playingRepeatedNote = false;
     }
     wrapped.noteOn(midiNote);
     silence = false;
@@ -57,7 +42,5 @@ public class RepeatedNoteAdaptor implements PlayerInterface {
     wrapped.noteOff(midiNote);
     lastNoteOff = midiNote;
     silence = true;
-    playingRepeatedNote = false;
-    repeatedNoteReductionMs = 0;
   }
 }
